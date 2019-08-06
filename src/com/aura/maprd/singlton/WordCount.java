1b1fb93a193b79dc4178edc5a82f1fb705d3c966
yz18710109979 <79241484@qq.com>
2019-08-06 13:43:19 +0800

    x

--- src/com/aura/maprd/singlton/WordCount.java
+++ src/com/aura/maprd/singlton/WordCount.java
@@ -1,19 +1,23 @@
 package com.aura.maprd.singlton;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
+ * javase实现
  * @author yz
  * @date 2019/8/3 11:01
  */
 public class WordCount {
-    public static void main(String[] args) throws Exception {
-    	Map<String, Integer> map01 = readOnFile("E:\\hadoop\\doc\\wc\\1.txt");
+    @SuppressWarnings("unchecked")
+	public static void main(String[] args) throws Exception {
+    	Map<String, Integer> map01 = readOnFile("D:\\doc\\wc\\1.txt");
     	System.out.println(map01);
+    	Map<String, Integer> resmap = mergeAllFile(map01);
+    	System.out.println(resmap);
     }
     //读取每个文件的信息并统计
     public static Map<String,Integer> readOnFile(String path) throws Exception{
@@ -35,8 +39,21 @@ public class WordCount {
         return map;
     }
     //汇总
-    public static Map<String,Integer> mergeAllFile(Map<String,Integer>...maps){
+    @SuppressWarnings("unchecked")
+	public static Map<String,Integer> mergeAllFile(Map<String,Integer>...maps){
         Map<String,Integer> resmap = new HashMap<>();
+        for (Map<String, Integer> map : maps) {
+			//keys
+        	Set<String> keys = map.keySet();
+        	for (String key : keys) {
+				if(!resmap.containsKey(key)) {
+					resmap.put(key, map.get(key));
+				}else {
+					int newvalue = resmap.get(key) + map.get(key);
+					resmap.put(key, newvalue);
+				}
+			}
+		}
         return resmap;
     }
 }
